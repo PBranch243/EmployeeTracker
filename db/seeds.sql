11f48079cbbe825bd2b74a208de23dc7b742617e
Phil Branch <philbranch243@gmail.com>
2022-07-28 19:10:38 -0500

    stopping for class

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, NULL),
+  ('Virginia', 'Woolf', 1, NULL),
+  ('Piers', 'Gaveston', 2, NULL),
+  ('Charles', 'LeRoi', 1, 1),
+  ('Katherine', 'Mansfield', 1, 1),
+  ('Dora', 'Carrington', 2, 1),
+  ('Edward', 'Bellamy', 2, 2),
+  ('Montague', 'Summers', 1, 2),
+  ('Octavia', 'Butler', 1, 2),
+  ('Unica', 'Zurn', 1, 1);
